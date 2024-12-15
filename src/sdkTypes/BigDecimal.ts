b2c49d34b03bba01b4e4405689db85fe1dd6d014
DKogan90 <david_kogan@hotmail.de>
2024-12-15 20:24:46 +0700

    changed BigDecimal to string

--- src/sdkTypes/BigDecimal.ts
+++ src/sdkTypes/BigDecimal.ts
@@ -1,15 +1,15 @@
 import {z} from "zod";
 
 // Define the schema using Zod
-export const BigDecimalSchema = z.number({message: "BigDecimal must be a number"});
+export const BigDecimalSchema = z.string({message: "BigDecimal must be a string"});
 
 // Type definition for the Money class based on the Zod schema
 export type BigDecimalType = z.infer<typeof BigDecimalSchema>;
 
 class BigDecimal {
-  value: number;
+  value: string;
 
-  constructor(value: number) {
+  constructor(value: string) {
     // Use the Zod schema to parse the input and validate it
     this.value = BigDecimalSchema.parse(value);
   }

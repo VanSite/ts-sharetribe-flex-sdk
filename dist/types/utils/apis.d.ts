3c8708bd6aec70073cf5c496f9f740ccfd37b737
DKogan90 <david_kogan@hotmail.de>
2025-03-05 12:05:03 +0700

    Remove dist directory from version control

--- dist/types/utils/apis.d.ts
+++
@@ -1,9 +0,0 @@
-import { ApiConfigs } from "../types/apiConfigs";
-/**
- * Creates API configurations for the SDK.
- *
- * @param isIntegrationSdk - Determines whether the configuration is for the Integration SDK (default: false).
- * @returns An object containing API configurations for the standard SDK or Integration SDK.
- */
-export declare const createApisConfigs: <I extends boolean = false>(isIntegrationSdk?: I) => ApiConfigs<I>;
-//# sourceMappingURL=apis.d.ts.map
--- dist/types/utils/stores/store.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generates a unique key for storing tokens.
- *
- * @param clientId - The unique identifier for the client.
- * @param namespace - The namespace to organize and scope the key.
- * @returns A string combining the namespace, client ID, and a token suffix.
- */
-export declare const generateKey: (clientId: string, namespace: string) => string;
-//# sourceMappingURL=store.d.ts.map
